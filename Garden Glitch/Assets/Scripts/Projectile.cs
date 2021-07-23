9bef96a2a341225ab8e03febe15d43da4413cd75
Cole <49996960+CWoulf@users.noreply.github.com>
2021-07-22 20:37:04 -0500

    ughh

---
+++ Garden Glitch/Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] float rotation = 360;
+    [SerializeField] float damage = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        /** Not yet since circles around gun point **/
+        //this.transform.Rotate(Vector3.forward* rotation * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var health = other.GetComponent<Health>();
+        var attacker = other.GetComponent<Attacker>();
+       
+        if (attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
