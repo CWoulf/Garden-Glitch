c2ebe08a191ba420334e6710f813a1ad22ff3b75
Cole <49996960+CWoulf@users.noreply.github.com>
2021-07-23 21:18:21 -0500

    added 2 defenders

--- Garden Glitch/Assets/Scripts/Shooter.cs
+++ Garden Glitch/Assets/Scripts/Shooter.cs
@@ -5,6 +5,52 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
+    Animator anim;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (isAttackerInLane())
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        else
+        {
+            anim.SetBool("isAttacking", false);
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+        foreach(AttackerSpawner spawner in spawners)
+        {
+            bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool isAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
     public void Fire()
     {
         Instantiate(projectile, gun.transform.position, Quaternion.identity);
